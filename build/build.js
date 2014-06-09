bc98d0003712d5eededc6dd13ffeabbf34e6c3c2
Matthew Mueller <mattmuelle@gmail.com>
2014-06-09 12:18:44 +0530

    commit build

---
+++ build/build.js
@@ -0,0 +1,669 @@
+
+/**
+ * Require the given path.
+ *
+ * @param {String} path
+ * @return {Object} exports
+ * @api public
+ */
+
+function require(path, parent, orig) {
+  var resolved = require.resolve(path);
+
+  // lookup failed
+  if (null == resolved) {
+    orig = orig || path;
+    parent = parent || 'root';
+    var err = new Error('Failed to require "' + orig + '" from "' + parent + '"');
+    err.path = orig;
+    err.parent = parent;
+    err.require = true;
+    throw err;
+  }
+
+  var module = require.modules[resolved];
+
+  // perform real require()
+  // by invoking the module's
+  // registered function
+  if (!module._resolving && !module.exports) {
+    var mod = {};
+    mod.exports = {};
+    mod.client = mod.component = true;
+    module._resolving = true;
+    module.call(this, mod.exports, require.relative(resolved), mod);
+    delete module._resolving;
+    module.exports = mod.exports;
+  }
+
+  return module.exports;
+}
+
+/**
+ * Registered modules.
+ */
+
+require.modules = {};
+
+/**
+ * Registered aliases.
+ */
+
+require.aliases = {};
+
+/**
+ * Resolve `path`.
+ *
+ * Lookup:
+ *
+ *   - PATH/index.js
+ *   - PATH.js
+ *   - PATH
+ *
+ * @param {String} path
+ * @return {String} path or null
+ * @api private
+ */
+
+require.resolve = function(path) {
+  if (path.charAt(0) === '/') path = path.slice(1);
+
+  var paths = [
+    path,
+    path + '.js',
+    path + '.json',
+    path + '/index.js',
+    path + '/index.json'
+  ];
+
+  for (var i = 0; i < paths.length; i++) {
+    var path = paths[i];
+    if (require.modules.hasOwnProperty(path)) return path;
+    if (require.aliases.hasOwnProperty(path)) return require.aliases[path];
+  }
+};
+
+/**
+ * Normalize `path` relative to the current path.
+ *
+ * @param {String} curr
+ * @param {String} path
+ * @return {String}
+ * @api private
+ */
+
+require.normalize = function(curr, path) {
+  var segs = [];
+
+  if ('.' != path.charAt(0)) return path;
+
+  curr = curr.split('/');
+  path = path.split('/');
+
+  for (var i = 0; i < path.length; ++i) {
+    if ('..' == path[i]) {
+      curr.pop();
+    } else if ('.' != path[i] && '' != path[i]) {
+      segs.push(path[i]);
+    }
+  }
+
+  return curr.concat(segs).join('/');
+};
+
+/**
+ * Register module at `path` with callback `definition`.
+ *
+ * @param {String} path
+ * @param {Function} definition
+ * @api private
+ */
+
+require.register = function(path, definition) {
+  require.modules[path] = definition;
+};
+
+/**
+ * Alias a module definition.
+ *
+ * @param {String} from
+ * @param {String} to
+ * @api private
+ */
+
+require.alias = function(from, to) {
+  if (!require.modules.hasOwnProperty(from)) {
+    throw new Error('Failed to alias "' + from + '", it does not exist');
+  }
+  require.aliases[to] = from;
+};
+
+/**
+ * Return a require function relative to the `parent` path.
+ *
+ * @param {String} parent
+ * @return {Function}
+ * @api private
+ */
+
+require.relative = function(parent) {
+  var p = require.normalize(parent, '..');
+
+  /**
+   * lastIndexOf helper.
+   */
+
+  function lastIndexOf(arr, obj) {
+    var i = arr.length;
+    while (i--) {
+      if (arr[i] === obj) return i;
+    }
+    return -1;
+  }
+
+  /**
+   * The relative require() itself.
+   */
+
+  function localRequire(path) {
+    var resolved = localRequire.resolve(path);
+    return require(resolved, parent, path);
+  }
+
+  /**
+   * Resolve relative to the parent.
+   */
+
+  localRequire.resolve = function(path) {
+    var c = path.charAt(0);
+    if ('/' == c) return path.slice(1);
+    if ('.' == c) return require.normalize(p, path);
+
+    // resolve deps by returning
+    // the dep in the nearest "deps"
+    // directory
+    var segs = parent.split('/');
+    var i = lastIndexOf(segs, 'deps') + 1;
+    if (!i) i = 0;
+    path = segs.slice(0, i + 1).join('/') + '/deps/' + path;
+    return path;
+  };
+
+  /**
+   * Check if module is defined at `path`.
+   */
+
+  localRequire.exists = function(path) {
+    return require.modules.hasOwnProperty(localRequire.resolve(path));
+  };
+
+  return localRequire;
+};
+require.register("component-query/index.js", Function("exports, require, module",
+"function one(selector, el) {\n\
+  return el.querySelector(selector);\n\
+}\n\
+\n\
+exports = module.exports = function(selector, el){\n\
+  el = el || document;\n\
+  return one(selector, el);\n\
+};\n\
+\n\
+exports.all = function(selector, el){\n\
+  el = el || document;\n\
+  return el.querySelectorAll(selector);\n\
+};\n\
+\n\
+exports.engine = function(obj){\n\
+  if (!obj.one) throw new Error('.one callback required');\n\
+  if (!obj.all) throw new Error('.all callback required');\n\
+  one = obj.one;\n\
+  exports.all = obj.all;\n\
+  return exports;\n\
+};\n\
+//@ sourceURL=component-query/index.js"
+));
+require.register("component-matches-selector/index.js", Function("exports, require, module",
+"/**\n\
+ * Module dependencies.\n\
+ */\n\
+\n\
+var query = require('query');\n\
+\n\
+/**\n\
+ * Element prototype.\n\
+ */\n\
+\n\
+var proto = Element.prototype;\n\
+\n\
+/**\n\
+ * Vendor function.\n\
+ */\n\
+\n\
+var vendor = proto.matches\n\
+  || proto.webkitMatchesSelector\n\
+  || proto.mozMatchesSelector\n\
+  || proto.msMatchesSelector\n\
+  || proto.oMatchesSelector;\n\
+\n\
+/**\n\
+ * Expose `match()`.\n\
+ */\n\
+\n\
+module.exports = match;\n\
+\n\
+/**\n\
+ * Match `el` to `selector`.\n\
+ *\n\
+ * @param {Element} el\n\
+ * @param {String} selector\n\
+ * @return {Boolean}\n\
+ * @api public\n\
+ */\n\
+\n\
+function match(el, selector) {\n\
+  if (vendor) return vendor.call(el, selector);\n\
+  var nodes = query.all(selector, el.parentNode);\n\
+  for (var i = 0; i < nodes.length; ++i) {\n\
+    if (nodes[i] == el) return true;\n\
+  }\n\
+  return false;\n\
+}\n\
+//@ sourceURL=component-matches-selector/index.js"
+));
+require.register("component-domify/index.js", Function("exports, require, module",
+"\n\
+/**\n\
+ * Expose `parse`.\n\
+ */\n\
+\n\
+module.exports = parse;\n\
+\n\
+/**\n\
+ * Wrap map from jquery.\n\
+ */\n\
+\n\
+var map = {\n\
+  legend: [1, '<fieldset>', '</fieldset>'],\n\
+  tr: [2, '<table><tbody>', '</tbody></table>'],\n\
+  col: [2, '<table><tbody></tbody><colgroup>', '</colgroup></table>'],\n\
+  _default: [0, '', '']\n\
+};\n\
+\n\
+map.td =\n\
+map.th = [3, '<table><tbody><tr>', '</tr></tbody></table>'];\n\
+\n\
+map.option =\n\
+map.optgroup = [1, '<select multiple=\"multiple\">', '</select>'];\n\
+\n\
+map.thead =\n\
+map.tbody =\n\
+map.colgroup =\n\
+map.caption =\n\
+map.tfoot = [1, '<table>', '</table>'];\n\
+\n\
+map.text =\n\
+map.circle =\n\
+map.ellipse =\n\
+map.line =\n\
+map.path =\n\
+map.polygon =\n\
+map.polyline =\n\
+map.rect = [1, '<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">','</svg>'];\n\
+\n\
+/**\n\
+ * Parse `html` and return the children.\n\
+ *\n\
+ * @param {String} html\n\
+ * @return {Array}\n\
+ * @api private\n\
+ */\n\
+\n\
+function parse(html) {\n\
+  if ('string' != typeof html) throw new TypeError('String expected');\n\
+  \n\
+  // tag name\n\
+  var m = /<([\\w:]+)/.exec(html);\n\
+  if (!m) return document.createTextNode(html);\n\
+\n\
+  html = html.replace(/^\\s+|\\s+$/g, ''); // Remove leading/trailing whitespace\n\
+\n\
+  var tag = m[1];\n\
+\n\
+  // body support\n\
+  if (tag == 'body') {\n\
+    var el = document.createElement('html');\n\
+    el.innerHTML = html;\n\
+    return el.removeChild(el.lastChild);\n\
+  }\n\
+\n\
+  // wrap map\n\
+  var wrap = map[tag] || map._default;\n\
+  var depth = wrap[0];\n\
+  var prefix = wrap[1];\n\
+  var suffix = wrap[2];\n\
+  var el = document.createElement('div');\n\
+  el.innerHTML = prefix + html + suffix;\n\
+  while (depth--) el = el.lastChild;\n\
+\n\
+  // one element\n\
+  if (el.firstChild == el.lastChild) {\n\
+    return el.removeChild(el.firstChild);\n\
+  }\n\
+\n\
+  // several elements\n\
+  var fragment = document.createDocumentFragment();\n\
+  while (el.firstChild) {\n\
+    fragment.appendChild(el.removeChild(el.firstChild));\n\
+  }\n\
+\n\
+  return fragment;\n\
+}\n\
+//@ sourceURL=component-domify/index.js"
+));
+require.register("component-indexof/index.js", Function("exports, require, module",
+"module.exports = function(arr, obj){\n\
+  if (arr.indexOf) return arr.indexOf(obj);\n\
+  for (var i = 0; i < arr.length; ++i) {\n\
+    if (arr[i] === obj) return i;\n\
+  }\n\
+  return -1;\n\
+};//@ sourceURL=component-indexof/index.js"
+));
+require.register("component-classes/index.js", Function("exports, require, module",
+"/**\n\
+ * Module dependencies.\n\
+ */\n\
+\n\
+var index = require('indexof');\n\
+\n\
+/**\n\
+ * Whitespace regexp.\n\
+ */\n\
+\n\
+var re = /\\s+/;\n\
+\n\
+/**\n\
+ * toString reference.\n\
+ */\n\
+\n\
+var toString = Object.prototype.toString;\n\
+\n\
+/**\n\
+ * Wrap `el` in a `ClassList`.\n\
+ *\n\
+ * @param {Element} el\n\
+ * @return {ClassList}\n\
+ * @api public\n\
+ */\n\
+\n\
+module.exports = function(el){\n\
+  return new ClassList(el);\n\
+};\n\
+\n\
+/**\n\
+ * Initialize a new ClassList for `el`.\n\
+ *\n\
+ * @param {Element} el\n\
+ * @api private\n\
+ */\n\
+\n\
+function ClassList(el) {\n\
+  if (!el) throw new Error('A DOM element reference is required');\n\
+  this.el = el;\n\
+  this.list = el.classList;\n\
+}\n\
+\n\
+/**\n\
+ * Add class `name` if not already present.\n\
+ *\n\
+ * @param {String} name\n\
+ * @return {ClassList}\n\
+ * @api public\n\
+ */\n\
+\n\
+ClassList.prototype.add = function(name){\n\
+  // classList\n\
+  if (this.list) {\n\
+    this.list.add(name);\n\
+    return this;\n\
+  }\n\
+\n\
+  // fallback\n\
+  var arr = this.array();\n\
+  var i = index(arr, name);\n\
+  if (!~i) arr.push(name);\n\
+  this.el.className = arr.join(' ');\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Remove class `name` when present, or\n\
+ * pass a regular expression to remove\n\
+ * any which match.\n\
+ *\n\
+ * @param {String|RegExp} name\n\
+ * @return {ClassList}\n\
+ * @api public\n\
+ */\n\
+\n\
+ClassList.prototype.remove = function(name){\n\
+  if ('[object RegExp]' == toString.call(name)) {\n\
+    return this.removeMatching(name);\n\
+  }\n\
+\n\
+  // classList\n\
+  if (this.list) {\n\
+    this.list.remove(name);\n\
+    return this;\n\
+  }\n\
+\n\
+  // fallback\n\
+  var arr = this.array();\n\
+  var i = index(arr, name);\n\
+  if (~i) arr.splice(i, 1);\n\
+  this.el.className = arr.join(' ');\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Remove all classes matching `re`.\n\
+ *\n\
+ * @param {RegExp} re\n\
+ * @return {ClassList}\n\
+ * @api private\n\
+ */\n\
+\n\
+ClassList.prototype.removeMatching = function(re){\n\
+  var arr = this.array();\n\
+  for (var i = 0; i < arr.length; i++) {\n\
+    if (re.test(arr[i])) {\n\
+      this.remove(arr[i]);\n\
+    }\n\
+  }\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Toggle class `name`, can force state via `force`.\n\
+ *\n\
+ * For browsers that support classList, but do not support `force` yet,\n\
+ * the mistake will be detected and corrected.\n\
+ *\n\
+ * @param {String} name\n\
+ * @param {Boolean} force\n\
+ * @return {ClassList}\n\
+ * @api public\n\
+ */\n\
+\n\
+ClassList.prototype.toggle = function(name, force){\n\
+  // classList\n\
+  if (this.list) {\n\
+    if (\"undefined\" !== typeof force) {\n\
+      if (force !== this.list.toggle(name, force)) {\n\
+        this.list.toggle(name); // toggle again to correct\n\
+      }\n\
+    } else {\n\
+      this.list.toggle(name);\n\
+    }\n\
+    return this;\n\
+  }\n\
+\n\
+  // fallback\n\
+  if (\"undefined\" !== typeof force) {\n\
+    if (!force) {\n\
+      this.remove(name);\n\
+    } else {\n\
+      this.add(name);\n\
+    }\n\
+  } else {\n\
+    if (this.has(name)) {\n\
+      this.remove(name);\n\
+    } else {\n\
+      this.add(name);\n\
+    }\n\
+  }\n\
+\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Return an array of classes.\n\
+ *\n\
+ * @return {Array}\n\
+ * @api public\n\
+ */\n\
+\n\
+ClassList.prototype.array = function(){\n\
+  var str = this.el.className.replace(/^\\s+|\\s+$/g, '');\n\
+  var arr = str.split(re);\n\
+  if ('' === arr[0]) arr.shift();\n\
+  return arr;\n\
+};\n\
+\n\
+/**\n\
+ * Check if class `name` is present.\n\
+ *\n\
+ * @param {String} name\n\
+ * @return {ClassList}\n\
+ * @api public\n\
+ */\n\
+\n\
+ClassList.prototype.has =\n\
+ClassList.prototype.contains = function(name){\n\
+  return this.list\n\
+    ? this.list.contains(name)\n\
+    : !! ~index(this.array(), name);\n\
+};\n\
+//@ sourceURL=component-classes/index.js"
+));
+require.register("flip/index.js", Function("exports, require, module",
+"/**\n\
+ * Flip template\n\
+ */\n\
+\n\
+var domify = require('domify');\n\
+var classes = require('classes');\n\
+var el = require('./template.js');\n\
+var matches = require('matches-selector');\n\
+\n\
+/**\n\
+ * Export `Flip`\n\
+ */\n\
+\n\
+module.exports = Flip;\n\
+\n\
+/**\n\
+ * Initialize `Flip`\n\
+ */\n\
+\n\
+function Flip() {\n\
+  if (!(this instanceof Flip)) return new Flip();\n\
+  this.el = domify(el);\n\
+  this.front = this.el.querySelector('.front');\n\
+  this.back = this.el.querySelector('.back');\n\
+  this.classes = classes(this.el.querySelector('.flip-card'));\n\
+  this.cards = [];\n\
+}\n\
+\n\
+/**\n\
+ * Add a card\n\
+ *\n\
+ * @param {Element} el\n\
+ * @return {Flip}\n\
+ * @api public\n\
+ */\n\
+\n\
+Flip.prototype.card = function(el) {\n\
+  var len = this.cards.length;\n\
+  if (!len) this.front.appendChild(el);\n\
+  else if (1 == len) this.back.appendChild(el);\n\
+  this.cards.push(el);\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Flip\n\
+ *\n\
+ * @param {Number|String} cls\n\
+ * @return {Flip} self\n\
+ * @api public\n\
+ */\n\
+\n\
+Flip.prototype.flip = function(cls) {\n\
+  var cards = this.cards;\n\
+  var card;\n\
+\n\
+  if ('string' == typeof cls) {\n\
+    for (var i = 0, card; card = cards[i]; i++) {\n\
+      if (matches(card, cls)) break;\n\
+    }\n\
+  } else {\n\
+    card = cards[cls];\n\
+  }\n\
+\n\
+  if (!card || card == this.front.firstChild) return this;\n\
+\n\
+  // update the back card\n\
+  this.back.textContent = '';\n\
+  this.back.appendChild(card);\n\
+\n\
+  // flip\n\
+  this.classes.toggle('flipped');\n\
+\n\
+  // change the state\n\
+  var front = this.front;\n\
+  this.front = this.back;\n\
+  this.back = front;\n\
+\n\
+  return this;\n\
+};\n\
+//@ sourceURL=flip/index.js"
+));
+require.register("flip/template.js", Function("exports, require, module",
+"module.exports = '<div class=\"flip\"> \\n\
+  <div class=\"flip-card\"> \\n\
+    <div class=\"face front\"></div> \\n\
+    <div class=\"face back\"></div> \\n\
+  </div>\\n\
+</div> \\n\
+';//@ sourceURL=flip/template.js"
+));
+
+
+
+
+
+
+require.alias("component-matches-selector/index.js", "flip/deps/matches-selector/index.js");
+require.alias("component-matches-selector/index.js", "matches-selector/index.js");
+require.alias("component-query/index.js", "component-matches-selector/deps/query/index.js");
+
+require.alias("component-domify/index.js", "flip/deps/domify/index.js");
+require.alias("component-domify/index.js", "domify/index.js");
+
+require.alias("component-classes/index.js", "flip/deps/classes/index.js");
+require.alias("component-classes/index.js", "classes/index.js");
+require.alias("component-indexof/index.js", "component-classes/deps/indexof/index.js");
+
+require.alias("flip/index.js", "flip/index.js");
